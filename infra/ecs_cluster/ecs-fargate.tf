5aefa3c02c78a4a5f5f95e2e22b4d6bf8229f817
sujon <sujon.sarkar31@gmail.com>
2022-12-05 01:12:01 -0500

    rename ecs

--- infra/ecs_cluster/ecs-fargate.tf
+++ infra/ecs_cluster/ecs-fargate.tf
@@ -32,31 +32,31 @@ resource "aws_alb" "sw-ecs_cluster_alb" {
   }
 }
 
-# resource "aws_alb_listener" "ecs_alb_https_listener" {
-#   load_balancer_arn = aws_alb.ecs_cluster_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate.ecs_domain_certificate.arn
+resource "aws_alb_listener" "sw-ecs_alb_https_listener" {
+  load_balancer_arn = aws_alb.sw-ecs_cluster_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate.sw-ecs_domain_certificate.arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.ecs_default_target_group.arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.sw-ecs_default_target_group.arn
+  }
 
-#   depends_on = [aws_alb_target_group.ecs_default_target_group]
-# }
+  depends_on = [aws_alb_target_group.sw-ecs_default_target_group]
+}
 
-# resource "aws_alb_target_group" "ecs_default_target_group" {
-#   name     = "${var.ecs_cluster_name}-TG"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = data.terraform_remote_state.infrastructure.outputs.vpc_id
+resource "aws_alb_target_group" "sw-ecs_default_target_group" {
+  name     = "${var.ecs_cluster_name}-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.infrastructure.outputs.vpc_id
 
-#   tags = {
-#     Name = "${var.ecs_cluster_name}-TG"
-#   }
-# }
+  tags = {
+    Name = "${var.ecs_cluster_name}-TG"
+  }
+}
 
 # resource "aws_route53_record" "ecs_load_balancer_record" {
 #   name = "*.${var.ecs_domain_name}"
